ac4c1594001e52e05c7124826ddbbe8f93edfdd0
shrijo <jonas.shriqui@gmail.com>
2019-12-02 16:40:31 +0100

    asdf

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ function getSliderValue(slider){
 
   else if (parseInt(slider.value, 10) >= 95 && parseInt(slider.value, 10) < 235){
     document.querySelector('.year').innerHTML = Math.round(2020 + 60 / 140 * (parseInt(slider.value, 10) - 95));
-    CABLES.patch.setVariable("frameGlacierOne", Math.floor( 111 + 164 / 140 * (parseInt(slider.value, 10)- 95), 10));
+    CABLES.patch.setVariable("frameGlacierOne", Math.floor( 112 + 164 / 140 * (parseInt(slider.value, 10)- 95), 10));
   }
 
   else if (parseInt(slider.value, 10) >=  235){
     document.querySelector('.year').innerHTML = Math.round(2080 + 40 / 45 * (parseInt(slider.value, 10) - 235));
-    CABLES.patch.setVariable("frameGlacierOne", Math.floor( 275 + 53 / 45 * (parseInt(slider.value, 10) - 234), 10));
+    CABLES.patch.setVariable("frameGlacierOne", Math.floor( 276 + 53 / 45 * (parseInt(slider.value, 10) - 235), 10));
   }
 
 }
